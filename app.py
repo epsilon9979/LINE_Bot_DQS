a197414ac993f87b703f65241de4680a9a3f1e3d
epsilon <jimmy9999great@gmail.com>
2025-01-09 01:21:21 +0800

    modified

--- app.py
+++ app.py
@@ -1,61 +1,55 @@
+# ======載入LineBot所需要的套件======
 from flask import Flask, request, abort
 
-from linebot.v3 import (
-    WebhookHandler
+from linebot import (
+    LineBotApi, WebhookHandler
 )
-from linebot.v3.exceptions import (
+from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
+from linebot.models import *
+
+# ======這裡是呼叫的py功能======
 import os
 
+# ======這裡是呼叫的檔案內容=====
+# from Message_test1 import *
+# from Message_test2 import *
+# from Message_test3 import *
 
+# ========ChatBot開始==========
 app = Flask(__name__)
 
-configuration = Configuration(access_token=os.getenv('Channel_Access_Token'))
-line_handler = WebhookHandler('Channel_Secret')
+# Channel Access Token(TOKEN)
+line_bot_api = LineBotApi(os.getenv('Channel_Access_Token'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('Channel_Secret'))
+# Your user ID
+line_bot_api.push_message(os.getenv('User_ID'), TextSendMessage(text='omg!終於成功了終於成功了！'))
 
-
-@app.route("/callback", methods=['POST'])
+# ========監聽所有來自 /callback 的 Post Request==========
+@app.route("/callback", methods=["POST"])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
+    signature = request.headers["X-Line-Signature"]
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
-        line_handler.handle(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
+    return "OK"
 
-    return 'OK'
-
-
-@line_handler.add(MessageEvent, message=TextMessageContent)
+# ========訊息傳遞區塊==========
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
-        )
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
 
+# ========主程式==========
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
