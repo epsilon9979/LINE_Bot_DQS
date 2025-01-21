965f3e6e4a814877a6b981d54d2052906b818313
epsilon <jimmy9999great@gmail.com>
2025-01-21 20:33:24 +0800

    modified

--- functions/questions.py
+++ functions/questions.py
@@ -5,13 +5,13 @@ import random
 def question(item):
     database = record()
     cursor, cnx = database.setting()
-    database.delete(cursor, cnx, item, "id = '1000'") #確保沒有臨時性作答紀錄
     
     counties = ['基隆','新北市','臺北市','桃園市','新竹','苗栗縣','臺中市','彰化縣','南投縣','雲林縣','嘉義','臺南市','高雄市','屏東縣','臺東縣','花蓮縣','宜蘭縣','連江縣','金門縣','澎湖縣','國際議題','能源議題','防災資訊']
     index = counties.index(item)
     which_table = ['Keelung', 'New_Taipei', 'Taipei', 'Taoyuan', 'Hsinchu', 'Miaoli', 'Taichung',
                     'Changhua', 'Nantou', 'Yunlin', 'Chiayi', 'Tainan', 'Kaohsiung', 'Pingtung',
                     'Taitung', 'Hualien', 'Yilan', 'Lienchiang', 'Kinmen', 'Penghu', 'international'][index]
+    database.delete(cursor, cnx, which_table, "id = '1000'") #確保沒有臨時性作答紀錄
     # if which_table not in database.show_tables(cursor):
     #     product = f"目前沒有 '{which_table}' 的相關題目"
     #     return product
