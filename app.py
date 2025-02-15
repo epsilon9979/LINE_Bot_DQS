75d98e7dc069d4607b1624cf026d763192c8bbf1
epsilon <jimmy9999great@gmail.com>
2025-02-15 16:18:32 +0800

    'final'

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ load_dotenv()
 app = Flask(__name__)
 line_bot_api = LineBotApi(os.getenv('Channel_Access_Token'))
 line_handler = WebhookHandler(os.getenv('Channel_Secret'))
-category = ['基隆','新北市','臺北市','桃園市','新竹','苗栗縣','臺中市','彰化縣','南投縣','雲林縣','嘉義','臺南市','高雄市','屏東縣','臺東縣','花蓮縣','宜蘭縣','連江縣','金門縣','澎湖縣','國際議題']
+category = ['基隆','新北市','臺北市','桃園市','新竹','苗栗縣','臺中市','彰化縣','南投縣','雲林縣','嘉義','臺南市','高雄市','屏東縣','臺東縣','花蓮縣','宜蘭縣','連江縣','金門縣','澎湖縣','國際議題','能源議題','防災資訊']
 # Your user ID
 # line_bot_api.push_message(os.getenv('User_ID'), TextSendMessage(text='他媽的終於成功了'))
 
