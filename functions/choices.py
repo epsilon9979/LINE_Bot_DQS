871af3eefbfbdb16667fd5f20333e3f8d4c7a9e7
epsilon <jimmy9999great@gmail.com>
2025-03-07 01:01:53 +0800

    modified

--- functions/choices.py
+++ functions/choices.py
@@ -36,10 +36,10 @@ def choice(data): # data = [TextSendMessage(questions), (id, questions, optionA,
             id_mem = id_mem + 1
     
     now = datetime.now() 
-    delta = timedelta(seconds=30)       
-    for time in database.fetch(cursor, cnx, "Memory", 'time', None):
-        if now - time[0] > delta:
-            database.delete(cursor, cnx, "Memory", f"time = {time[0]}")
+    # delta = timedelta(seconds=30)       
+    # for time in database.fetch(cursor, cnx, "Memory", 'time', None):
+    #     if now - time[0] > delta:
+    #         database.delete(cursor, cnx, "Memory", f"time = {time[0]}")
     question_2 = (id_mem, data[1][1], random_options[0][1], random_options[1][1], random_options[2][1],
                   random_options[3][1], answer, data[1][7], data[1][8], now, data[1][10])
     database.append(cursor, cnx, question_2, "Memory")
